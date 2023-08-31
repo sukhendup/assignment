52a20da981bde6056c433a7546f938b062bdc599
sukhendup <sukhendupatra007@gmail.com>
2023-09-01 03:03:23 +0530

    final commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const Card = (
-    {title, 
+const Card = ({
+    title,
     desc,
     img,
     date,
@@ -9,49 +9,52 @@ const Card = (
     orgicon,
     venname,
     vencity,
-    vencntry} = props) => {
-  return (
-    <div className="flex w-1/2 flex-col items-center rounded-md border mx-2 my-2 md:w-1/3 bg-gray-400 shadow-2xl hover:shadow-slate-500">
-      <div className="h-full w-full">
-        <img
-          src={img}
-          alt="Image failed to load"
-          className="h-full w-full rounded-md object-fit"
-        />
-      </div>
-      <div>
-        <div className="p-4">
-          <h1 className="inline-flex items-center text-lg font-semibold underline">
-            {title}
-          </h1>
-          <p class="mt-3 text-sm font-semibold text-blue-900">
-            {desc}
-          </p>
-          <div className="mt-4">
-            <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-bold text-gray-900 border-2 border-black">
-            📅{date}
-            </span>
-            <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-bold text-gray-900 border-2 border-black">
-            📌{venname},{vencity}
-            </span>
-            <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-bold text-gray-900 border-2 border-black">
-            🏳️{vencntry}
-            </span>
-          </div>
-      <div className="mt-3 flex items-center space-x-2">
-        <img
-          className="inline-block h-8 w-8 rounded-full border-2"
-          src={orgicon}
-          alt="Image failed to load"
-        />
-        <span className="flex flex-col">
-          <span className="font-black text-gray-900 text-sm">{orgname}</span>
-        </span>
-      </div>
-    </div>
-  </div>
-</div>
-  )
-}
+    vencntry,
+} = props) => {
+    return (
+        <div className="flex w-1/2 flex-col items-center rounded-md mx-2 my-2 md:w-1/3 bg-gradient-to-r from-gray-200 via-gray-400 to-gray-600 shadow-slate-500 shadow-xl font-mono">
+            <div className="h-full w-full">
+                <img
+                    src={img}
+                    alt="Image failed to load"
+                    className="h-full w-full rounded-md object-fit"
+                />
+            </div>
+            <div>
+                <div className="p-4">
+                    <h1 className="inline-flex items-center text-lg font-semibold underline">
+                        {title}
+                    </h1>
+                    <p className="mt-3 text-sm font-semibold text-blue-900">
+                        {desc}
+                    </p>
+                    <div className="mt-4">
+                        <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-bold text-gray-900 border-2 border-black">
+                            📅{date}
+                        </span>
+                        <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-bold text-gray-900 border-2 border-black">
+                            📌{venname},{vencity}
+                        </span>
+                        <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-bold text-gray-900 border-2 border-black">
+                            🏳️{vencntry}
+                        </span>
+                    </div>
+                    <div className="mt-3 flex items-center space-x-2">
+                        <img
+                            className="inline-block h-8 w-8 rounded-full border-2"
+                            src={orgicon}
+                            alt="Image failed to load"
+                        />
+                        <span className="flex flex-col">
+                            <span className="font-black text-gray-900 text-sm">
+                                {orgname}
+                            </span>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Card
+export default Card;
--- src/components/Conference.jsx
+++ src/components/Conference.jsx
@@ -1,46 +1,79 @@
-import React from 'react'
-import { useState,useEffect } from "react"
-import axios from "axios"
-import { server } from '../App'
-import Card from './Card'
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { server } from "../App";
+import Card from "./Card";
 
 const Conference = () => {
-    const [data,setData] = useState([])
-    useEffect(()=>{
-    axios.get(`${server}`)
-    .then((res)=>{
-        const {data} = res.data.content;
-        setData(data)
-        console.log(data)
-    })
-    .catch((e)=>{
-        console.log(e)
-    })
-  },[])
-  return (
-    <>
-    <div className=" h-32 flex justify-center items-center bg-slate-800 ">
-      <div className="font-black text-3xl text-white border-4 p-4 rounded-full
-      shadow-md shadow-white">: CONFERENCE PAGE :</div>
-    </div>
-    
-    <div className= "flex flex-wrap content-center flex-col md:flex-row md:justify-evenly gap-3 bg-slate-800">
-      {data.map((i) => <Card
-        key={i.id}
-        title ={i.title}
-        desc = {i.description}
-        img = {i.banner_image}
-        date = {i.date_time}
-        orgname = {i.organiser_name}
-        orgicon = {i.organiser_icon}
-        venname = {i.venue_name}
-        vencity = {i.venue_city}
-        vencntry = {i.venue_country}
-      />)
-      }
-    </div>
-    </>
-  )
-}
+    const [data, setData] = useState([]);
+    const [search, setSearch] = useState("");
 
-export default Conference
+    const handleSearch = async (search) => {
+        const res = await axios.get(`${server}`);
+        const { data } = await res.data.content;
+        console.log(data);
+        if (search.length != 0) {
+            let myArr = data.filter((i) =>
+                i.title.toLowerCase().includes(search.toLowerCase())
+            );
+            if (myArr.length != 0) {
+                console.log(myArr);
+                setData(myArr);
+            }
+        } else {
+            setData(data);
+        }
+    };
+    useEffect(() => {
+        handleSearch("");
+    }, []);
+
+    return (
+        <>
+            <div className="h-32 flex flex-row justify-evenly items-center bg-gray-500 font-mono">
+                <div
+                    className=" font-black text-3xl text-white border-4 p-4 rounded-full
+      shadow-md shadow-white"
+                >
+                    CONFERENCE PAGE
+                </div>
+                <div className="flex self flex-row justify-end gap-3">
+                    <input
+                        type="text"
+                        placeholder="Search by Title"
+                        value={search}
+                        onChange={(e) => {
+                            setSearch(e.target.value);
+                        }}
+                        className=" bg-black text-white border-2 border-black px-5 py-2 outline-none shadow-slate-700 shadow-xl rounded-xl"
+                    />
+                    <button
+                        className=" bg-black text-white border-2 border-black px-5 py-2 rounded-full shadow-slate-700 shadow-xl"
+                        onClick={() => handleSearch(search)}
+                    >
+                        Search
+                    </button>
+                </div>
+            </div>
+
+            <div className="flex flex-wrap content-center flex-col md:flex-row md:justify-evenly gap-3 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+                {data.map((i) => (
+                    <Card
+                        key={i.id}
+                        title={i.title}
+                        desc={i.description}
+                        img={i.banner_image}
+                        date={i.date_time}
+                        orgname={i.organiser_name}
+                        orgicon={i.organiser_icon}
+                        venname={i.venue_name}
+                        vencity={i.venue_city}
+                        vencntry={i.venue_country}
+                    />
+                ))}
+            </div>
+        </>
+    );
+};
+
+export default Conference;
